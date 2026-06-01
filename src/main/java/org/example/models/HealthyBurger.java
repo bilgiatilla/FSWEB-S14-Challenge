48ee5591062283d9951e1c28f4d6ff45a6ce2601
bilgiatilla <atillabilgi.ab@gmail.com>
2026-06-02 00:24:19 +0300

    Create Hamburger, HealtyBurger, and DeluxeBurger classes

---
+++ src/main/java/org/example/models/HealthyBurger.java
@@ -0,0 +1,45 @@
+package org.example.models;
+
+public class HealthyBurger extends Hamburger{
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
+
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+    public HealthyBurger(String name, double price, String breadRollType) {
+        super(name, "Tofu", price, breadRollType);
+    }
+
+    public void addHealthyAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+    public void addHealthyAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+    @Override
+    public double itemizeHamburger() {
+        double totalPrice = super.itemizeHamburger();;
+
+        System.out.println("Name: " + this.getName());
+        System.out.println("Meat: " + this.getMeat());
+        System.out.println("BreadRollType: " + this.getBreadRollType());
+
+        totalPrice = super.itemizeHamburger();
+
+        if (healthyExtra1Name != null) {
+            System.out.println("HealthyAddition1: " + healthyExtra1Name);
+            totalPrice += healthyExtra1Price;
+        }
+
+        if (healthyExtra2Name != null) {
+            System.out.println("HealthyAddition2: " + healthyExtra2Name);
+            totalPrice += healthyExtra2Price;
+        }
+        setPrice(totalPrice);
+        System.out.println("Price: " + getPrice());
+
+        return getPrice();
+    }
+}
